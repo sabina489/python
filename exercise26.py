fd85de0619d4fb447f1d1bf73f9bbd9b10bca851
sabina489 <Karkisabina869@gmail.com>
2021-11-22 14:14:51 +0545

    utf-8 encodes the string

--- exercise26.py
+++ exercise26.py
@@ -27,25 +27,21 @@ print(txt_again_read())
 
 # Strings, Bytes and Character Encodings
 print("Let's practice everything.")
-print('You need to know about escape with \\ that do:')
+print('You need to know about escapes with \\ that do:')
 print('\n newlines and \t tabs.')
 
+poem = """ The lovely world
+7 with logic so firmly planted
+8 cannot discern \n the needs of love
+9 nor comprehend passion from intuition
+10 and requires an explanation
+11 \n\t\twhere there is none."""
 
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explanation
-\n\t\twhere there is none.
-"""
-
-print("--------------)
+print("--------")
 print(poem)
-print("--------------")
+print("------")
 
-
-five = 10 - 2 + 3 - 1
+five = 5 - 3 + 3 - 1
 print(f"This should be five: {five}")
 
 def secret_formula(started):
@@ -54,26 +50,26 @@ def secret_formula(started):
     crates = jars / 100
     return jelly_beans, jars, crates
 
-
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
-# remember that this is another way to format a string
-print("With a starting point of: {}".format(start_point))
+#Another way to format a string
+print("With a starting point of: {}". format(start_point))
 
-# it's just like with an f"" string
+#Its just like with an f""" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
-# this is an easy way to apply a list to a format string
-print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+formula = secret_formula(start_point)
+
+#This is an easy way to apply a list to a format string
+print("We'd have {} beans, {} jars, and {} crates".format(*formula))
 
 
 
-people = 20
+'''people = 20
 cates = 30
 dogs = 15
 
@@ -101,5 +97,5 @@ if people <= dogs
 
 
 if people = dogs:
-    print("People are dogs.")
+    print("People are dogs.")'''
 
