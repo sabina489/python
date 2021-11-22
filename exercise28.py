f7883a5e4b494f36af918992610f4a3d21e8d8d4
sabina489 <Karkisabina869@gmail.com>
2021-11-22 14:58:20 +0545

    if statement

---
+++ exercise28.py
@@ -0,0 +1,44 @@
+# Memorizing Logic
+''' The Truth Terms
+and
+or
+not
+!=(not equal)
+==(equal)
+>=(greate than equal)
+<=(less than equal)
+True
+False
+'''
+
+# If-Statement
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs :
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs.")
+
