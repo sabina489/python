930944c312832d9d9fb68e71bbfc7aabb7ae0f28
sabina489 <Karkisabina869@gmail.com>
2021-11-23 14:57:06 +0545

    branches_and_functions

---
+++ exercise34.py
@@ -0,0 +1,9 @@
+# Accessing Elements of Lists
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#print the first animal is at 0 and is a bear
+
+print("The first(1st) animal is at 0 and is a bear.")
+print("The third animal is at 2 and is a peacock.")
+print("The fifth animal is at 4 and is a platypus")
